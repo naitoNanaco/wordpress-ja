5f77e014cbb584e2faf454034347f341489b084d
naitoNanaco <yohei@Niyo.local>
2019-03-03 11:47:54 +0900

    debug

--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker tag $1 ${DOCKER_USERNAME}/$1
-docker tag $1 ${DOCKER_USERNAME}/${VERSION}-$1
+image_name='wordpress-ja:'; tag_v=${1//${image_name}/${image_name}${VERSION}-}
+docker tag $1 ${DOCKER_USERNAME}/${tag_v}
 docker push ${DOCKER_USERNAME}/$1
-docker push ${DOCKER_USERNAME}/${VERSION}-$1
+docker push ${DOCKER_USERNAME}/${tag_v}
